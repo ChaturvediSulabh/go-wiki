5b51db2d19fcf00f68dbaa2aeb1bd2175063a8c6
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-05-03 19:53:27 +0100

    Developed Func to Load a file from the disk

--- wiki.go
+++ wiki.go
@@ -1,7 +1,10 @@
 package main
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+)
 
+//Page to render
 type Page struct {
 	Title string
 	Body  []byte
@@ -9,6 +12,18 @@ type Page struct {
 
 func (p *Page) save() error {
 	return ioutil.WriteFile(p.Title+".html", p.Body, 0600)
+}
+
+func load(fileName string) (*Page, error) {
+	body, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	p := Page{
+		Title: fileName,
+		Body:  body,
+	}
+	return &p, nil
 }
 
 func main() {
