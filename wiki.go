141ed5aff5b15e7e4200589bf3389158d5458586
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-05-03 19:41:48 +0100

    developed func (p *page) save() error {}

--- wiki.go
+++ wiki.go
@@ -1,14 +1,15 @@
 package main
 
+import "io/ioutil"
+
 type Page struct {
 	Title string
 	Body  []byte
 }
 
 func (p *Page) save() error {
-	return nil
+	return ioutil.WriteFile(p.Title+".html", p.Body, 0600)
 }
 
 func main() {
-
 }
