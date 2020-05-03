b5de96e9b3604bf8534713dc425651939d37601c
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-05-03 20:01:27 +0100

    Update main function to save and load the html page

--- wiki.go
+++ wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 )
 
 //Page to render
@@ -27,4 +28,15 @@ func load(fileName string) (*Page, error) {
 }
 
 func main() {
+	p1 := Page{
+		"wiki",
+		[]byte("<h1>Hello World!</h1>"),
+	}
+	p1.save()
+	p2, err := load(p1.Title + ".html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println("FileName:", p2.Title)
+	// fmt.Printf("Content:-\n%v", string(p2.Body))
 }
